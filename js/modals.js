fefe02150b2fc0aa3e7e2329cd1618b75a159975
bribrode <bribrode@umich.edu>
2019-04-20 18:49:53 -0400

    Add files via upload

---
+++ js/modals.js
@@ -0,0 +1,110 @@
+// Modals ------------------------------------
+var modal1 = document.getElementById('myModal1');
+var modal2 = document.getElementById('myModal2');
+var modal3 = document.getElementById('myModal3');
+var modal4 = document.getElementById('myModal4');
+var modal5 = document.getElementById('myModal5');
+var modal6 = document.getElementById('myModal6');
+var modal7 = document.getElementById('myModal7');
+var modal8 = document.getElementById('myModal8');
+var modal9 = document.getElementById('myModal9');
+var modal10 = document.getElementById('myModal10');
+var modal11 = document.getElementById('myModal11');
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+var btn4 = document.getElementById("myBtn4");
+var btn5 = document.getElementById("myBtn5");
+var btn6 = document.getElementById("myBtn6");
+var btn7 = document.getElementById("myBtn7");
+var btn8 = document.getElementById("myBtn8");
+var btn9 = document.getElementById("myBtn9");
+var btn10 = document.getElementById("myBtn10");
+var btn11 = document.getElementById("myBtn11");
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementById("close1");
+var span2 = document.getElementById("close2");
+var span3 = document.getElementById("close3");
+var span4 = document.getElementById("close4");
+var span5 = document.getElementById("close5");
+var span6 = document.getElementById("close6");
+var span7 = document.getElementById("close7");
+var span8 = document.getElementById("close8");
+var span9 = document.getElementById("close9");
+var span10 = document.getElementById("close10");
+var span11 = document.getElementById("close11");
+
+// When the user clicks on the button, open the modal
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+btn4.onclick = function() {
+  modal4.style.display = "block";
+}
+btn5.onclick = function() {
+  modal5.style.display = "block";
+}
+btn6.onclick = function() {
+  modal6.style.display = "block";
+}
+btn7.onclick = function() {
+  modal7.style.display = "block";
+}
+btn8.onclick = function() {
+  modal8.style.display = "block";
+}
+btn9.onclick = function() {
+  modal9.style.display = "block";
+}
+btn10.onclick = function() {
+  modal10.style.display = "block";
+}
+btn11.onclick = function() {
+  modal11.style.display = "block";
+}
+
+
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+span4.onclick = function() {
+  modal4.style.display = "none";
+}
+span5.onclick = function() {
+  modal5.style.display = "none";
+}
+span6.onclick = function() {
+  modal6.style.display = "none";
+}
+span7.onclick = function() {
+  modal7.style.display = "none";
+}
+span8.onclick = function() {
+  modal8.style.display = "none";
+}
+span9.onclick = function() {
+  modal9.style.display = "none";
+}
+span10.onclick = function() {
+  modal10.style.display = "none";
+}
+span11.onclick = function() {
+  modal11.style.display = "none";
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Sticky Navigation ----------------------------------------------------------
+
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
@@ -17,7 +19,7 @@ function myFunction() {
   }
 }
 
-///////////
+// Accordian ----------------------------------------------------------
 var acc = document.getElementsByClassName("facility");
 var i;
 
@@ -37,32 +39,18 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-/////////////
-var slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+// Form Validation ------------------------------------------------------------
+function validateForm() {
+  var x = document.forms["contactform"]["name"].value;
+  var y = document.forms["contactform"]["email"].value;
+  if (x == "") {
+    alert("Please enter your name before submitting");
+    return false;
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" curr", "");
+  if (y == ""){
+    alert("Please enter your email before submitting");
+    return false;
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " curr";
 }
